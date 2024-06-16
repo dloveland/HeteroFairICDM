518f0116c1b2f81ace97f4f47bda08c19f0fc278
Donald Loveland <donaldloveland1@yahoo.com>
2024-06-15 18:45:11 -0700

    Update models_reg.py

--- models_reg.py
+++ models_reg.py
@@ -10,7 +10,7 @@ from copy import deepcopy
 from torch_geometric.nn.conv.sage_conv import SAGEConv
 import types
 import scipy
-from torch_geometric.nn import GCNConv, SGConv, GATConv, JumpingKnowledge, APPNP, GCN2Conv, MessagePassing, FAConv
+from torch_geometric.nn import GCNConv
 
 
 
@@ -33,11 +33,7 @@ class Model(nn.Module):
                 self.layers.append(Linear(nhid, nhid))
             elif model_type == 'sage':
                 self.layers.append(SAGE_Layer(nhid))
-            elif model_type == 'fagcn':
-                self.layers.append(FAGCN_layer(nhid, eps=float(additional_params[0])))
-            elif model_type == 'gcnii':
-                self.layers.append(GCNII_Layer(nhid, d, alpha=float(additional_params[0]), theta=float(additional_params[1])))
-
+           
         self.output_linear = nn.Linear(in_features=nhid, out_features=nout)
 
         # additional layers for training 
@@ -51,15 +47,10 @@ class Model(nn.Module):
         x = self.input_linear(x)
         x = self.act(x)
         x = self.dropout(x)
- 
-        # residual connection back to initial features 
-        if self.model_type in ['fagcn', 'gcnii']:
-            x_0 = x.detach().clone() 
+
 
         for layer in self.layers:
-            if self.model_type in ['fagcn', 'gcnii']:
-                x = layer(x, x_0, edge_index)
-            elif self.model_type == 'mlp':
+            if self.model_type == 'mlp':
                 x = layer(x)
             else:
                 x = layer(x, edge_index)
@@ -97,39 +88,3 @@ class SAGE_Layer(nn.Module):
         x = self.layer(x, edge_index)
         return x
 
-
-########### GCNII #############
-
-class GCNII_Layer(nn.Module):
-    def __init__(self, dim, layer, alpha=0.1, theta=0.5, shared_weights=True):
-        super().__init__()
-
-        self.layer = GCN2Conv(dim, alpha, theta, layer + 1, shared_weights, normalize=False)
-
-    def forward(self, x, x_0, edge_index):
-        
-        x = self.layer(x, x_0, edge_index)
-        return x
-
-########### FAGCN #############
-class FAGCN_layer(torch.nn.Module):
-    def __init__(self, dim, eps=0.1):
-        super(FAGCN_layer, self).__init__()
-        self.layer = FAConv(dim, eps)
- 
-    def forward(self, x, x_0, edge_index):
-        x = self.layer(x, x_0, edge_index)
-        return x
- 
-
-class SGC(torch.nn.Module):
-    def __init__(self, nfeat, nclass, depth):
-        super(SGC, self).__init__()
-        self.sgc_conv = SGConv(nfeat, nclass, depth)
-
-    def reset_parameters(self):
-        self.sgconc.reset_parameters()
-
-    def forward(self, x, edge_index):
-        x = self.sgc_conv(x, edge_index)
-        return x 
